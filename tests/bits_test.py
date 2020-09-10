17dfd2cd98bfd421946d196c29a2e83d445afbda
GlebMorgan <glebmorgan@gmail.com>
2020-09-10 03:23:42 +0300

    feat(Bits): add .compose(), .extract(), .extract2(), .pack() + tests + docs
    bits_test.py - add check_result() to perform actual assertions

--- tests/bits_test.py
+++ tests/bits_test.py
@@ -1,6 +1,9 @@
+from inspect import signature
+from operator import itemgetter
+
 from utils import Bits
 from pytest import mark, fixture, raises
-from random import choice
+from random import choice, randint
 
 
 @mark.skip(reason='methods signature change')
@@ -132,6 +135,19 @@ class TestSet:
             Bits(42).set(bit)
 
 
+def check_result(method: str, operand, args, expected, *,
+                 unpackargs: bool = False, subtype: type = False, kwargs: dict = None):
+    if not kwargs:
+        kwargs = {}
+    if unpackargs is False:
+        args = (args,)
+    result = getattr(Bits(operand), method)(*args, **kwargs)
+    assert result == expected
+    assert type(result) is type(expected)
+    if result and subtype:
+        assert all(type(num) == subtype for num in result)
+
+
 class TestSetClear:
 
     patterns = (
@@ -169,19 +185,16 @@ class TestSetClear:
 
     @fixture(params=patterns, ids=lambda par: ' '.join(par.split()))
     def data_set_bits(self, request):
-        operand, args, result = request.param.split()
-        yield int(operand, 2), *tuple(int(n) for n in args), int(result, 2)
+        operand, args, result = request.param.strip().split()
+        yield int(operand, 2), tuple(int(n) for n in args), Bits(result, 2)
 
     @fixture(params=map(invert_bits.__func__, patterns), ids=lambda par: '-'.join(par.split()))
     def data_clear_bits(self, request):
-        operand, args, result = request.param.split()
-        yield int(operand, 2), *tuple(int(n) for n in args), int(result, 2)
+        operand, args, result = request.param.strip().split()
+        yield int(operand, 2), tuple(int(n) for n in args), Bits(result, 2)
 
-    def test_set_bits(self, data_set_bits):
-        operand, *bits, expected = data_set_bits
-        result = Bits(operand).set(*bits)
-        assert result == expected
-        assert type(result) is Bits
+    def test_set(self, data_set_bits):
+        check_result('set', *data_set_bits, unpackargs=True)
 
     @mark.parametrize('bit', ('0', '123', None, 1.0))
     def test_set_invalid_bit(self, bit):
@@ -193,14 +206,11 @@ class TestSetClear:
         assert result == Bits(42)
         assert type(result) is Bits
 
-    def test_clear_bits(self, data_clear_bits):
-        operand, *bits, expected = data_clear_bits
-        result = Bits(operand).clear(*bits)
-        assert result == expected
-        assert type(result) is Bits
+    def test_clear(self, data_clear_bits):
+        check_result('clear', *data_clear_bits, unpackargs=True)
 
     @mark.parametrize('bit', ('0', '123', None, 1.0))
-    def test_set_invalid_bit(self, bit):
+    def test_clear_invalid_bit(self, bit):
         with raises(TypeError):
             Bits(42).clear(bit)
 
@@ -232,22 +242,25 @@ class TestMask:
         '1001 1--- 1001',
     )
 
-    random_marker_patterns = tuple(
-            pattern.replace('-', choice('!@#$%^&*()_+?|/~123abcXYZ.'))
-            for pattern in patterns if '-' in pattern
-    )
-    patterns_all = patterns + random_marker_patterns
+    delimiters = '!@#$%^&*()_+?|/~abcXYZ. '
 
-    @fixture(params=patterns_all, ids=lambda par: ' '.join(par.split()))
+    @fixture(params=patterns)
     def data_mask(self, request):
-        operand, mask, result = request.param.split()
-        yield int(operand, 2), mask, int(result, 2)
+        operand, mask, result = request.param.strip().split()
+        yield int(operand, 2), mask, Bits(result, 2)
+
+    @fixture(params=patterns)
+    def data_mask_delim(self, request):
+        operand, mask, result = request.param.strip().split()
+        if '-' in mask:
+            mask = mask.replace('-', choice(self.delimiters))
+        yield int(operand, 2), mask, Bits(result, 2)
 
     def test_mask(self, data_mask):
-        operand, mask, expected = data_mask
-        result = Bits(operand).mask(mask)
-        assert result == expected
-        assert type(result) is Bits
+        check_result('mask', *data_mask)
+
+    def test_mask_delim(self, data_mask_delim):
+        check_result('mask', *data_mask_delim)
 
     @mark.parametrize('mask', (0, Bits, None, 1.0))
     def test_mask_invalid(self, mask):
@@ -277,10 +290,7 @@ class TestFlag:
         yield int(operand, 2), int(pos), bool(int(result))
 
     def test_flag(self, data_flag):
-        operand, pos, expected = data_flag
-        result = Bits(operand).flag(pos)
-        assert result == expected
-        assert type(result) is bool
+        check_result('flag', *data_flag)
 
 
 class TestFlags:
@@ -297,12 +307,165 @@ class TestFlags:
 
     @fixture(params=patterns, ids=lambda par: ' '.join(par.split()))
     def data_flags(self, request):
-        operand, n, result = request.param.split()
+        operand, n, result = request.param.strip().split()
         yield int(operand, 2), int(n), tuple(bool(int(bit)) for bit in result[1:-1])
 
     def test_flags(self, data_flags):
-        operand, n, expected = data_flags
-        result = Bits(operand).flags(n)
-        assert result == expected
-        assert type(result) is tuple
-        assert type(result[0]) is bool if len(result) != 0 else result == ()
+        check_result('flags', *data_flags, subtype=bool)
+
+
+class TestExtract:
+
+    patterns = (
+        ('00110101      1111  (101)       ', 'single num'),
+        ('00110110    112233  (11 01 10)  ', 'multiple num'),
+        ('00011001   111223-  (1 10 0)    ', 'delim'),
+        ('00000010  11111111  (10)        ', 'single number'),
+        ('11100011  --111     (11)        ', 'delim before'),
+        ('11100011  111--     (0)         ', 'delim after'),
+        ('10000011  11222333  (10 0 11)   ', 'no delims'),
+        ('10000101  44423331  (1 0 10 100)', 'not in order'),
+        ('00110101  -3331112  (10 1 11)   ', 'not in order + delim'),
+        ('10000101  5555-001  (10 1 1000) ', 'missed markers'),
+        ('11000001  11----22  (11 1)      ', 'long gap'),
+        ('01100101  --112---  (10 0)      ', 'delim at the ends'),
+        ('00000001  -------9  (1)         ', 'long delim'),
+        ('01000100  -1-2-3-4  (1 0 1 0)   ', 'single bit nums'),
+    )
+
+    @fixture(params=patterns, ids=itemgetter(1))
+    def data_extract(self, request, res_type):
+        operand, mask, result = request.param[0].strip().split(maxsplit=2)
+        yield int(operand, 2), mask, res_type(int(num, 2) for num in result[1:-1].split())
+
+    @fixture(params=patterns, ids=itemgetter(1))
+    def data_extract_sep(self, request, method, res_type):
+        operand, mask, result = request.param[0].strip().split(maxsplit=2)
+        sep_count = randint(1, 5)
+        sep = signature(getattr(Bits, method)).parameters['sep'].default
+        markers = list(mask)
+        for i in range(sep_count):
+            markers.insert(randint(0, len(mask)), sep)
+        yield int(operand, 2), ''.join(markers), res_type(int(num, 2) for num in result[1:-1].split())
+
+    @mark.parametrize('method, res_type', [('extract', tuple), ('extract2', list)])
+    def test_extract(self, data_extract, method, res_type):
+        check_result(method, *data_extract, subtype=int)
+
+    @mark.parametrize('method, res_type', [('extract', tuple), ('extract2', list)])
+    def test_extract_sep(self, data_extract_sep, method, res_type):
+        check_result(method, *data_extract_sep, subtype=int)
+
+    @mark.parametrize('method', ('extract', 'extract2'))
+    def test_extract_duplicate_markers(self, method):
+        extract = getattr(Bits(42), method)
+        with raises(ValueError):
+            extract('11-21')
+
+    @mark.parametrize('method, res_type', [('extract', tuple), ('extract2', list)])
+    def test_extract_empty(self, method, res_type):
+        extract = getattr(Bits(42), method)
+        assert extract('') == res_type()
+        assert extract('---') == res_type()
+        assert extract('@') == res_type()
+
+    @mark.parametrize('sep', ('', ' ', '@', 'sep', False), ids=lambda par: f'sep={par}')
+    @mark.parametrize('method, res_type', [('extract', tuple), ('extract2', list)])
+    def test_extract_custom_sep(self, method: str, res_type: type, sep: str):
+        value = 0b00110101
+        mask = '{sep}1333{sep}22--'.format(sep=sep if sep else '')
+        expected = res_type((0, 1, 0b11))
+        check_result(method, value, mask, expected, subtype=int, kwargs={'sep': sep})
+
+
+class TestCompose:
+
+    patterns = ('1101001', '1000000', '111', '000', '1', '0', '')
+
+    @fixture(params=patterns)
+    def data_compose(self, request):
+        pattern = request.param
+        flags = tuple(bool(int(bit)) for bit in pattern) if pattern else ()
+        result = Bits(''.join(reversed(pattern)), 2) if pattern else Bits()
+        yield 0, flags, Bits(result)
+
+    def test_compose(self, data_compose):
+        check_result('compose', *data_compose, unpackargs=True)
+
+    def test_compose_int(self):
+        check_result('compose', 0, (1, 1, 0, 1, 0, 0), Bits(0b1011), unpackargs=True)
+
+    def test_compose_generator(self):
+        def gen_flags():
+            for flag in '110100':
+                yield bool(int(flag))
+        check_result('compose', 0, gen_flags(), Bits(0b1011), unpackargs=True)
+
+
+class TestPack:
+
+    patterns = (
+        ('10011010  -00--1--  11 1       11111110', 'general case'),
+        ('00001000  000       101        00001101', 'single'),
+        ('00001000  000--     101        00010100', 'single + delim'),
+        ('00001000  -000--    101        00010100', 'single + 2x delim'),
+        ('00010001  0         0          00010000', 'single bit'),
+        ('00110011  000-----  10         01010011', 'start'),
+        ('11001100  ------00  11         11001111', 'end'),
+        ('11100010  00-11--2  00 11 1    00111011', 'triple'),
+        ('00000000  0------1  1 0        10000000', 'edges'),
+        ('10101010  00011222  10 10 101  01010101', 'no gaps'),
+        ('11110000  01222234  0 1 1 0 1  01000101', 'bits'),
+        ('10011100  -00-1-00  11 0       11110111', 'dups'),
+        ('10100010  00000000  11111111   11111111', 'whole range'),
+        ('00000001  000--11-  1 11       00100111', 'expand to left'),
+        ('00001111  0111-11-  1 101      11011011', 'different group lengths'),
+        ('10100001  -         1 1011 0   10100001', 'empty'),
+    )
+
+    @fixture(params=patterns, ids=itemgetter(1))
+    def data_pack(self, request):
+        operand, mask, *args, result = request.param[0].strip().split()
+        yield int(operand, 2), (mask, *(int(num, 2) for num in args)), Bits(result, 2)
+
+    @fixture
+    def data_pack_sep(self, data_pack):
+        operand, args, result = data_pack
+        mask, *args = args
+        sep_count = randint(1, 5)
+        sep = signature(Bits.pack).parameters['sep'].default
+        markers = list(mask)
+        for i in range(sep_count):
+            markers.insert(randint(0, len(mask)), sep)
+        yield operand, (''.join(markers), *args), result
+
+    def test_pack(self, data_pack):
+        check_result('pack', *data_pack, unpackargs=True)
+
+    def test_pack_sep(self, data_pack_sep):
+        check_result('pack', *data_pack_sep, unpackargs=True)
+
+    def test_pack_empty_nums(self):
+        with raises(ValueError):
+            Bits(42).pack('00--')
+
+    def test_pack_empty_mask(self):
+        assert Bits().pack('', 1, 2, 3) == Bits()
+
+    def test_pack_empty_args(self):
+        assert Bits(42).pack('') == Bits(42)
+        assert Bits(42).pack('---') == Bits(42)
+        assert Bits(42).pack('rubbish') == Bits(42)
+
+    @mark.parametrize('sep', ('', ' ', '@', 'sep', False), ids=lambda par: f'sep={par}')
+    def test_pack_custom_sep(self, sep):
+        value = 0b10
+        mask = '{sep}0011{sep}22--'.format(sep=sep if sep else '')
+        nums = (0b11, 0, 1)
+        expected = Bits(0b1100_0110)
+        check_result('pack', value, (mask, *nums), expected, unpackargs=True, kwargs={'sep': sep})
+
+    @mark.parametrize('mask, args', (('00112200--3-', '1 2 10'), ('11--', '0')), ids=itemgetter(0))
+    def test_pack_invalid(self, mask, args):
+        with raises(ValueError):
+            Bits(42).pack(mask, *(int(num) for num in args))
--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
+
+import re
 from functools import reduce
+from itertools import groupby
 from operator import or_, and_
-from typing import Tuple
+from typing import Tuple, List
+
 
 # TODO: short module description, purpose
 
@@ -12,6 +16,7 @@ class Bits(int):
         """
         Set bits on specified 'positions' (set to 1)
         Position indexes start from zero and are counted from rightmost bit to the left
+
         >>> Bits(0b01).set(1) == Bits(0b11)
         >>> Bits(0b1010).set(0, 2) == Bits(0b1111)
         >>> Bits(0b0).set(3) == Bits(0b1000)
@@ -29,6 +34,7 @@ class Bits(int):
         """
         Clear bits on specified 'positions' (set to 0)
         Position indexes start from zero and are counted from rightmost bit to the left
+
         >>> Bits(0b01).clear(0) == Bits(0b0)
         >>> Bits(0b1010).clear(3) == Bits(0b0010)
         >>> Bits(0b11).clear(0, 2) == Bits(0b10)
@@ -47,6 +53,7 @@ class Bits(int):
         Set/clear bits according to positions of 1 and 0 in 'mask' string
         1 sets the corresponding bit, 0 clears the bit, - (dash) leaves
             current bit unchanged (any character may be used instead of -)
+
         >>> Bits(0b0101).mask('-01-') == Bits(0b0011)
         >>> Bits(0b1100).mask('11') == Bits(0b1111)
         >>> Bits(0b1111).mask('-') == Bits(0b1111)
@@ -70,19 +77,122 @@ class Bits(int):
     def flags(self, n) -> Tuple[bool, ...]:
         """
         Convert 'n' rightmost bits to tuple of booleans
-        Resulting order is right to left
-        >>> Bits(0b0011).flags(3) == (True, True, False)
+        Functionally is the inverse of .compose() method
+        Resulting flags order conforms to default bit indexing - right to left
+        >>> Bits(0b0100).flags(3) == (False, False, True)
         """
         return tuple(bool((self >> i) & 0b1) for i in range(n))
 
-    def place(self, what, pos):
-        pass
+    @classmethod
+    def compose(cls, *flags: bool) -> Bits:
+        """
+        Construct a Bits object out of given sequence of bits specified in 'flags'
+        Functionally is the inverse of .flags() method
+        The bits placing order conforms to default bit indexing - right to left
+        >>> Bits.compose(False, False, True, False) == Bits(0x100)
+        """
+        return cls(reduce(or_, (bit << i for i, bit in enumerate(flags) if bit), cls()))
 
-    def extract(self, mask):
-        # Mask may look like '8----2--'
-        pass
+    def extract(self, mask: str, *, sep: str = ' ') -> Tuple[int, ...]:
+        """
+        Pull out one or multiple values on sub-byte basis according to 'mask'
+        Mask consists of 3 types of markers:
+            • number marker - digits 0..9
+            • delimiter - any character (except for digit or specified separator character)
+            • separator - character specified by 'sep' (could not be another marker character)
+        Each group of adjacent digits holds position of a separate number to be extracted
+            from current Bits object (represented as a bit sequence)
+        The order of extraction is defined by value of marker digits themselves
+        Multiple groups with the same digit would raise a ValueError
+        Delimiter characters denote positions of the bits to be skipped (dash is recommended)
+        Separator characters are intended to enhance readability and are simply ignored
+        Mask is right-aligned with processed number (to match least-significant bit)
 
-    @staticmethod
-    def compose():
-        # former bitsarray()
-        pass
+        >>> Bits(0b0001_1100).extract('111--') == (0b111,)
+        >>> Bits(0b1100_0110).extract('2221 11-3') == (0b1, 0b110, 0b0)
+        >>> Bits(0b0011_1010).extract('1122 2-13') -> ValueError  # duplicating marker group: 1
+        """
+
+        if sep:
+            mask = ''.join(mask.split(sep))
+        try:
+            n = max(int(m) for m in mask if m.isdecimal())
+        except ValueError:
+            return ()
+        result = [...] * (n + 1)
+        pos = len(mask)
+        for marker, group in groupby(mask):
+            size = len(tuple(group))
+            pos -= size
+            if marker.isdecimal():
+                i = int(marker)
+                if result[i] is not Ellipsis:
+                    raise ValueError(f"Duplicate mask marker group: {marker}")
+                result[i] = self >> pos & (2 ** size - 1)
+        return tuple(num for num in result if num is not Ellipsis)
+
+    def extract2(self, mask: str, *, sep: str = ' ') -> List[int, ...]:
+        """
+        A 20% slower implementation with same functionality
+        This variant also returns a list instead of tuple
+        """
+
+        if sep:
+            mask = ''.join(mask.split(sep))
+        try:
+            n = int(max(filter(str.isdecimal, mask)))
+        except ValueError:
+            return []
+        result = []
+        for marker in ''.join(map(str, range(n+1))):
+            if marker in mask:
+                before, group, *after = re.split(rf'({marker}+)', mask)
+                if len(after) > 1:
+                    raise ValueError(f"Duplicate mask marker group: {marker}")
+                result.append(self >> len(after[0]) & (2 ** len(group) - 1))
+        return result
+
+    def pack(self, mask: str, *nums: int, sep: str = ' ') -> Bits:
+        """
+        Insert values specified in 'nums' into current Bits object
+        Functionally is the inverse of .extract() method
+            on specified positions denoted by 'mask'
+        Mask consists of 3 types of markers:
+            • index marker - digits from 0 to <len of 'nums' argument>
+            • delimiter - any character (except for digit or specified separator character)
+            • separator - character specified by 'sep' (could not be another marker character)
+        Each group of adjacent digits holds position of a separate number to be inserted
+            into current Bits object (represented as a bit sequence)
+        The value of marker digits themselves defines 0-based index of number
+            from 'nums' argument to be inserted into designated position
+        Multiple groups of index markers with the same digit would cause corresponding
+            number to be inserted into multiple positions, possibly stripping it
+            to the length of its respective group if required
+        Delimiter characters denote positions of the bits to be skipped (dash is recommended)
+        Separator characters are intended to enhance readability and are simply ignored
+        Mask is right-aligned with processed number (to match least-significant bit)
+
+        >>> Bits().pack('0000--1-', 0b100, 1) == Bits(0b0100_0010)
+        >>> Bits(0b10).pack('0011 22--', 0b11, 0, 1) == Bits(0b1100_0110)
+        >>> Bits(0b1001_1010).pack('00- 111-', 0b11, 1) == Bits(0b1111_0010)
+        """
+
+        if sep:
+            mask = ''.join(mask.split(sep))
+        result = 0  # holds all nums packed together sitting on their corresponding positions
+        result_mask = 0  # holds 1s on those bit positions where something was packed
+        pos = len(mask)
+        for marker, group in groupby(mask):
+            num_size = len(tuple(group))
+            pos -= num_size
+            if marker.isdecimal():
+                i = int(marker)
+                try:
+                    num_mask = 2 ** num_size - 1
+                    result |= (nums[i] & num_mask) << pos
+                    result_mask |= num_mask << pos
+                except IndexError:
+                    raise ValueError(f"Invalid mask index marker: {marker}. "
+                                     f"Indexes should start from 0 "
+                                     f"and not exceed the count of inserted values") from None
+        return Bits(self & ~result_mask | result)
