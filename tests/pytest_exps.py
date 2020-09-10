956d99235c7f936c052eb8dfe1e6030ea8f1bc35
GlebMorgan <glebmorgan@gmail.com>
2020-09-10 03:25:53 +0300

    tweak(pytest_exps): add exp with indirect fixture parametrization

--- tests/pytest_exps.py
+++ tests/pytest_exps.py
@@ -1,11 +1,13 @@
-from pytest import fixture
+from pytest import fixture, mark
 
+
+# ————————————————————————————————— Fixture parametrization via @fixture(params=...) ————————————————————————————————— #
 
 fixture_params = ((1, 0), (2, 5))
 
 
 @fixture(params=fixture_params, ids=lambda par: f"{par[0]} and {par[1]}")
-def fixture_parametrized(request):
+def fixture_with_params(request):
     print('\n<fixture startup>')
     print(f'Request object: {request}')
     print(f'Request attrs:')
@@ -14,22 +16,38 @@ def fixture_parametrized(request):
     print('\n<fixture teardown>')
 
 
-def test_fixture_parametrize(fixture_parametrized):
-    arg = fixture_parametrized
+def test_fixture_parametrize(fixture_with_params):
+    arg = fixture_with_params
     assert isinstance(arg, tuple)
 
 
-# ———————————————————————————————————————————————————————————————————————————————————————————————————————————————————— #
-
+# ————————————————————————————————————————————————— Nested fixtures —————————————————————————————————————————————————— #
 
 fixture_nested_params = (1, 7)
 
 
 @fixture(params=fixture_nested_params)
-def fixture_nested(request, fixture_parametrized):
-    yield sum(fixture_parametrized), request.param
+def fixture_nested(request, fixture_with_params):
+    yield sum(fixture_with_params), request.param
 
 
 def test_fixture_nested(fixture_nested):
     arg = fixture_nested
-    assert arg[0] == arg[1]
+    assert 1
+
+
+# ——————————————————————————— Fixture parametrization via @mark.parametrize(indirect=True) ——————————————————————————— #
+
+@fixture(params=('a', 'b', 'c', 'd', 'e'))
+def fixture_parametrize(request, arg):
+    print()
+    print(f'{arg=}, {request.param=}')
+    yield arg+1
+
+
+@mark.parametrize('arg', (1, 2, 3, 4, 5))
+def test_fixture_parametrize(fixture_parametrize, arg):
+    assert type(fixture_parametrize) is int
+
+
+# ———————————————————————————————————————————————————————————————————————————————————————————————————————————————————— #
